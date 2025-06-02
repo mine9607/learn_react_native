e9bb337472c6aa6d22777bc43aabe7fbe7b9e638
mine9607 <81383389+mine9607@users.noreply.github.com>
2025-06-02 16:51:32 -0400

    Added an async function to access and select images from the device media library using the expo-image-picker library and connected the function to the primary image picker button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,35 @@
 import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
